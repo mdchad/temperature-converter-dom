c652d5f75fe692bcead8b526fe01bbb925af60b4
Irsyad <irsyad.muhd@gmail.com>
2016-08-19 02:07:54 +0800

    some changes to css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello, front end');
+function setCelsiusToF() {
+var cel=document.getElementById("celsius")
+var far=document.getElementById("fahrenheit")
+
+var result = celsius.value * 1.8 + 32;
+fahrenheit.textContext = result;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("DOM loaded");
+
+  // attach a click handler to the set title button
+  document.getElementById("submit").addEventListener("click", function() {
+    console.log("set title button clicked");
+    setCelsiusToF();
+  });
